b1b771db7e1d20e23ef797f9356201daa9feab1e
FinnikXCF <hydrosanek@mail.ru>
2019-11-04 01:55:30 +0300

    so effort to intro

--- src/Intro.svelte
+++ src/Intro.svelte
@@ -1,21 +1,23 @@
-<h1>The X-COM Files</h1>
+<div class="centreinfo">
+  <h1>The X-COM Files</h1>
 
-<h3>Welcome to The X-COM Files mod to OpenXcom.</h3>
-<br/>
-<h4>What is XCF?</h4>
-This megamod is a global conversion to OpenXcom (clone of the classic UFO:Enemy Unknown 1994), preserving the original setting.<br/> 
-<br/>
-With this mod the game starts in 1996. The player gets control over the newly formed organization X-Com - a project of the UN, designed to investigate abnormal events on our planet. The style of the series X-Files (where the name comes from) is surprisingly clear. In the beginning, no fighters, dozens of scientists, millions of dollars in budgets. You only have two special agents and they travel by public transport! Investigate mysterious incidents, tie the evidence together to shed light on extraterrestrial activities, develop X-COM and prepare for an alien invasion from Cidonia!<br/>
-This mod contains many, many additions, like new mission types, enemies, equipment and story arcs (including the entirety of my previous project, the Final Mod Pack). It also places the game a bit more firmly in the 90's conspiracy genre and its clichés.<br/>
-<br/>
-<a href="https://openxcom.mod.io/the-x-com-files">Download mod</a>, join our community with <a href="https://discord.gg/XrQQTPg">Discord server</a> and <a href="https://openxcom.org/forum/index.php/board,21.0.html">forum!</a><br/>
-<br/>
-Current mod version is 1.0
-<pre>
-"Now, this doesn't at all mean that this is the end. I will continue releasing new versions just like before, at least for the foreseeable future...<br/>
-The 1.0 only means that I am satisfied with how the mod works now and consider it "done" enough to say so."
-</pre>
-<div>– Solarius Scorch, mod author</div> 
-<br/>
-<br/>
-Powered by <a href="https://github.com/baturinsky/xpedia/">Baturinsky's XPedia</a>.
+  Welcome to The X-COM Files mod to OpenXcom.
+  <br/>
+  <h4>What is XCF?</h4>
+  This megamod is a global conversion to OpenXcom (clone of the classic UFO:Enemy Unknown 1994), preserving the original setting.<br/> 
+    <br/>
+    With this mod the game starts in 1996. The player gets control over the newly formed organization X-Com - a project of the UN, designed to investigate abnormal events on our planet. The style of the series X-Files (where the name comes from) is surprisingly clear. In the beginning, no fighters, dozens of scientists, millions of dollars in budgets. You only have two special agents and they travel by public transport! Investigate mysterious incidents, tie the evidence together to shed light on extraterrestrial activities, develop X-COM and prepare for an alien invasion from Cidonia!<br/>
+    This mod contains many, many additions, like new mission types, enemies, equipment and story arcs (including the entirety of my previous project, the Final Mod Pack). It also places the game a bit more firmly in the 90's conspiracy genre and its clichés.<br/>
+    <br/>
+    <a href="https://openxcom.mod.io/the-x-com-files">Download mod</a>, join our community with <a href="https://discord.gg/XrQQTPg">Discord server</a> and <a href="https://openxcom.org/forum/index.php/board,21.0.html">forum!</a><br/>
+    <br/>
+  <h4>Current mod version is 1.0</h4>
+  <pre style="with: 50%">"Now, this doesn't at all mean that this is the end. I will continue releasing new versions just like before, at least for the foreseeable future...
+  The 1.0 only means that I am satisfied with how the mod works now and consider it "done" enough to say so."</pre>
+  – Solarius Scorch, mod author, lead designer
+  <br/>
+  <br/>
+
+  Wiki is powered by <a href="https://github.com/baturinsky/xpedia/">Baturinsky's XPedia</a>.
+</div>
+
