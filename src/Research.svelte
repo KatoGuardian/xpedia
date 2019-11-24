eb86df22dee667c7324a79afadf38cc19de490bd
KatoGuardian <50804689+KatoGuardian@users.noreply.github.com>
2019-11-24 15:29:26 +0700

    Subheaders localization

--- src/Research.svelte
+++ src/Research.svelte
@@ -20,7 +20,7 @@
   </tr>
   {#each Object.entries(research).sort((a, b) => (longLists.includes(b[0])?-10:10) + (a[0] > b[0] ? 1 : -1)) as [key, prop]}
     {#if longLists.includes(key) && prop && prop.length>0}
-      <tr><td colspan="2" class="table-subheader">{rul.decamelize(key)}</td></tr>
+      <tr><td colspan="2" class="table-subheader">{rul.str(key)}</td></tr>
       <tr><td colspan="2" class="cols" style="columns:2;">
         <ul class="research-links">
           {#each prop as field, i}
